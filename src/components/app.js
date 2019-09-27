3afdd6c568d2d1adbfd0130138647b23c3a26f49
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-09-27 13:54:34 -0400

    Implements get meal  button

--- src/components/app.js
+++ src/components/app.js
@@ -6,12 +6,36 @@ import getRandomMeal from '../utilities/networking.js';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      meal: null
+    }
   }
 
+  getMeal() {
+    getRandomMeal()
+      .then((meal) => {
+        this.setState((state) => {
+          return { meal: meal }
+        });
+      });
+  }
+
+  // comments on using one style of getMeal (along with getRandomMeal in networking.js) over the other?
+  // getMeal() {
+  //   let callback = function(meal) {
+  //     this.setState((state) => {
+  //       return { meal: meal }
+  //     });
+  //   }
+
+  //   getRandomMeal(callback.bind(this));
+  // }
+
   render() {
-    getRandomMeal();
+
     return (
       <div>
+        <button onClick={this.getMeal.bind(this)}>Get meal</button>
         <Directions></Directions>
         <Ingredients></Ingredients>
       </div>
--- src/utilities/networking.js
+++ src/utilities/networking.js
@@ -1,13 +1,20 @@
 let url = `https://www.themealdb.com/api/json/v1/1/random.php`;
 
 let getRandomMeal = () => {
-  let meal;
-  fetch(url)
+  return fetch(url)
   .then((res) => res.json())
   .then((res) => {
-    meal = res.meals[0];
+    return res.meals[0];
   });
-  return meal;
 }
+
+// let getRandomMeal = (callback) => {
+//   let meal = fetch(url)
+//   .then((res) => res.json())
+//   .then((res) => {
+//     meal = res.meals[0];
+//     callback(meal);
+//   });
+// }
 
 export default getRandomMeal;
