3c4eddd8b3d144999a25dfba2e4149c9afeec4db
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-10-03 19:45:59 -0400

    Updates

---
+++ precache-manifest.9e3eaea720795d97cc6e2e4fd946ec38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7c913dd9be8604d758f4e19ab4c6918",
+    "url": "/what-should-i-eat/index.html"
+  },
+  {
+    "revision": "01c57ad0dc72e573a161",
+    "url": "/what-should-i-eat/static/css/main.55063a8e.chunk.css"
+  },
+  {
+    "revision": "289ca154b68c57865f69",
+    "url": "/what-should-i-eat/static/js/2.351a2c06.chunk.js"
+  },
+  {
+    "revision": "01c57ad0dc72e573a161",
+    "url": "/what-should-i-eat/static/js/main.9c59b8e6.chunk.js"
+  },
+  {
+    "revision": "534537b977123c4ab9aa",
+    "url": "/what-should-i-eat/static/js/runtime-main.1bd73be4.js"
+  }
+]);
